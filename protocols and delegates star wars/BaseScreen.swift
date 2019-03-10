8f10616c6610c0f41c12594487adab77a05aa3c0
hard2want <thedukeofchicago@gmail.com>
2019-03-09 19:35:34 -0600

    protocol and delegate created to enable side selection to be displayed on the base view

--- protocols and delegates star wars/BaseScreen.swift
+++ protocols and delegates star wars/BaseScreen.swift
@@ -22,6 +22,26 @@ class BaseScreen: UIViewController {
     
     @IBAction func chooseButtonTapped(_ sender: UIButton) {
         let selectionVC = storyboard?.instantiateViewController(withIdentifier: "SelectionScreen") as! SelectionScreen
+        
+        // here is where the 'doer' of the delegates instructions is performed
+        // selectionVC is the identifier we just established
+        // selectionDelegate is the identifier we created in the SelectionScreen
+        //
+        selectionVC.selectionDelegate = self
+        
         present(selectionVC, animated: true, completion: nil)
     }// end chooseButtonTapped()
 } // end BaseScreenVC {}
+
+// you can extend a class's functionality via extensions
+// since we're conforming to the protocol we need the protocol's function that we defined
+extension BaseScreen: SideSelectionDelegate {
+    
+    // when the button is tapped, either imperial or rebel, in the selection screen it is calling this function
+    // all the information this function needs to fill the parameter values are passed as the arguments from the selection screen
+    func didTapChoice(image: UIImage, name: String, color: UIColor) {
+        mainImageView.image = image
+        nameLabel.text = name
+        view.backgroundColor = color        
+    } // end didTapChoice()
+} // end extension BaseScreen
--- protocols and delegates star wars/SelectionScreen.swift
+++ protocols and delegates star wars/SelectionScreen.swift
@@ -23,10 +23,12 @@ class SelectionScreen: UIViewController {
         // selectionDelegate is who is going to receive the instructions
         // didTapChoice() is the function required via the protocol we created
         // pass in the desired values for the arguement requirements
-        
+        // NOTE: you can force unwrap the image because you 100% know it is in your assets folder, but if pulling image from the web that link may not always work so you would use a conditional
+        selectionDelegate.didTapChoice(image: UIImage(named: "darth")!, name: "Darth Vader", color: .red)
           } // end imperialButtonTapped()
     
     @IBAction func rebelButtonTapped(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
+        selectionDelegate.didTapChoice(image: UIImage(named: "yoda")!, name: "Yoda", color: .cyan)
     } // end rebelButtonTapped()
 } // end SelectionScreen{}
